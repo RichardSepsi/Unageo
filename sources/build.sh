f7d347976bf5a38403a67a401f7f5d20665d49bb
Richard Sepši <appsepsi@gmail.com>
2024-01-13 11:59:33 +0100

    Smaller update
    Updated metrics (more spacing)
    slightly changed letter e
    updated build.sh

--- sources/build.sh
+++ sources/build.sh
@@ -1,2 +1,7 @@
 fontmake -m Unageo.designspace -o variable --output-path Unageo[wght,ital].ttf --keep-overlaps
 gftools fix-nonhinting Unageo[wght,ital].ttf Unageo[wght,ital].ttf
+fonttools varLib.instancer ./Unageo[wght,ital].ttf wght=300:900 ital=0
+rm Unageo[wght,ital]-backup-fonttools-prep-gasp.ttf
+mv Unageo[wght,ital]-instance.ttf Unageo[wght].ttf
+fonttools varLib.instancer ./Unageo[wght,ital].ttf wght=300:900 ital=1
+mv Unageo[wght,ital]-instance.ttf Unageo-italic[wght].ttf
